725613fd50e6b971ee46f461a0e5a2adfd93b8ab
Yihang Ho <hoyihang5@gmail.com>
2013-09-04 11:37:56 +0800

    Handle JSON and XML request using the default _serialize key method.

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,9 +1,11 @@
 <?php
 class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
+    public $components = array('RequestHandler');
 
     public function index() {
     	$this->set('posts', $this->Post->find('all'));
+    	$this->set('_serialize', array('posts'));
 	}
 }
 ?>

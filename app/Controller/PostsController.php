86a31c3b531b8e4dab5c082c809572972cc127cc
Yihang Ho <hoyihang5@gmail.com>
2013-09-04 11:34:43 +0800

    Add model and controller for Post. Also define the index action and its corresponding view.

---
+++ app/Controller/PostsController.php
@@ -0,0 +1,9 @@
+<?php
+class PostsController extends AppController {
+    public $helpers = array('Html', 'Form');
+
+    public function index() {
+    	$this->set('posts', $this->Post->find('all'));
+	}
+}
+?>
